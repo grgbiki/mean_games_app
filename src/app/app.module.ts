7fda75ea5c4313e7da90ba7323a138f2a868652f
grgbiki <grgbiki53@gmail.com>
2022-01-19 20:39:08 -0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GamesComponent,
+    FooterComponent,
+    NavigatorComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: HomeComponent
+      }, {
+        path: "games",
+        component: GamesComponent
+      }, {
+        path: "**",
+        component: ErrorPageComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
